e2d0d9e8f64be1ca8016bea712d975dcd1957886
DerrickMbugua <derrickmbugua50@gmail.com>
2022-05-05 10:45:08 +0300

    components and props

--- src/App.js
+++ src/App.js
@@ -5,30 +5,105 @@ function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
 }
 
+function formatDate(date) {
+  return date.toLocaleDateString();
+}
+
+function Comment(props) {
+  return (
+    <div className="Comment">
+      <UserInfo user={info.user}/>
+      <div className="Comment-text">
+        {props.text}
+      </div>
+      <div className="Comment-date">
+        {formatDate(props.date)}
+      </div>
+   </div>
+  );
+}
+
+const comment = {
+  date: new Date(),
+  text: 'I hope you enjoy learning React!',
+  author: {
+    name: 'Hello Kitty',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+};
+
+function Avatar(props) {
+  return (
+    <img className="Avatar"
+      src={props.user.avatarUrl}
+      alt={props.user.name}
+    />
+  );
+}
+
+const avatar = {
+  user: {
+    avatarUrl: 'http://placekitten.com/g/64/64',
+    name: 'Only one Dero',
+    
+  } 
+}
+
+function UserInfo(props) {
+  return (
+    <div className="UserInfo">
+      <Avatar user={props.user} />
+      <div className="UserInfo-name">
+        {props.user.name}
+      </div>
+    </div>
+  );
+}
+
+const info={
+  user:{
+    name:'Big man',
+    avatarUrl: 'http://placekitten.com/g/64/64'
+  }
+}
+
+
 function App() {
-  const name = "Derrick Mwema";
+  const name = 'Derrick Mwema';
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React
         </a>
       </header>
       <body>
+       
+        <div>
+        <Welcome name='Sara' />
+        <Welcome name='Mwema' />
+        <Welcome name='Dero' />
+        </div>
+
+        <div>
+          <Comment 
+          date={comment.date}
+          text={comment.text}
+          author={comment.author}
+           />
+        </div>
         
-      <h1>Hello, {name}</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      <Welcome name="Sara" />
+        
       </body>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,29 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user) {
-  return user.firstName + ' ' + user.lastName;
-}
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-const user = {
-  firstName: 'Harper',
-  lastName: 'Perez'
-};
-
-
-const element = (
-  <div>
-    <h1>
-    Hello, {formatName(user)}!
-  </h1>
-  <h2>It is {new Date().toLocaleTimeString()}.</h2>
-  </div>
-  
-);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -35,7 +12,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
-//  root.render(element);
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))

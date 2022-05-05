c4f452897f52abbe887278478002f1af1ef1731a
DerrickMbugua <derrickmbugua50@gmail.com>
2022-05-05 16:43:33 +0300

    feat: state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function Welcome(props) {
   return <h1>Hello, {props.name}</h1>;
@@ -11,15 +12,11 @@ function formatDate(date) {
 
 function Comment(props) {
   return (
-    <div className="Comment">
-      <UserInfo user={info.user}/>
-      <div className="Comment-text">
-        {props.text}
-      </div>
-      <div className="Comment-date">
-        {formatDate(props.date)}
-      </div>
-   </div>
+    <div className='Comment'>
+      <UserInfo user={info.user} />
+      <div className='Comment-text'>{props.text}</div>
+      <div className='Comment-date'>{formatDate(props.date)}</div>
+    </div>
   );
 }
 
@@ -28,16 +25,13 @@ const comment = {
   text: 'I hope you enjoy learning React!',
   author: {
     name: 'Hello Kitty',
-    avatarUrl: 'http://placekitten.com/g/64/64'
-  }
+    avatarUrl: 'http://placekitten.com/g/64/64',
+  },
 };
 
 function Avatar(props) {
   return (
-    <img className="Avatar"
-      src={props.user.avatarUrl}
-      alt={props.user.name}
-    />
+    <img className='Avatar' src={props.user.avatarUrl} alt={props.user.name} />
   );
 }
 
@@ -45,39 +39,56 @@ const avatar = {
   user: {
     avatarUrl: 'http://placekitten.com/g/64/64',
     name: 'Only one Dero',
-    
-  } 
-}
+  },
+};
 
 function UserInfo(props) {
   return (
-    <div className="UserInfo">
+    <div className='UserInfo'>
       <Avatar user={props.user} />
-      <div className="UserInfo-name">
-        {props.user.name}
-      </div>
+      <div className='UserInfo-name'>{props.user.name}</div>
     </div>
   );
 }
 
-const info={
-  user:{
-    name:'Big man',
-    avatarUrl: 'http://placekitten.com/g/64/64'
+const info = {
+  user: {
+    name: 'Big man',
+    avatarUrl: 'http://placekitten.com/g/64/64',
+  },
+};
+
+function Sum(a, b) {
+  return a + b;
+}
+
+function Withdraw(total, amount) {
+  total -= amount;
+  return total;
+}
+
+const values = {
+  a: 1,
+  b: 2,
+};
+
+class Clock extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {date: new Date()}
+  }
+  render() {
+    return (
+      <div>
+        <h1>
+          <h1>Hello, {this.state.date.toLocaleDateString()}</h1>
+        </h1>
+      </div>
+    );
   }
 }
 
-function Sum(a, b){
-  return a + b
-}
-
-const values={
-  a: 1,
-  b: 2
-}
-
 function App() {
-
   return (
     <div className='App'>
       <header className='App-header'>
@@ -95,22 +106,22 @@ function App() {
         </a>
       </header>
       <body>
-       <h1>{Sum(values.a,values.b)}</h1>
+        <Clock />
+        <h1>{Sum(values.a, values.b)}</h1>
+        <h1>{Withdraw(5, 2)}</h1>
         <div>
-        <Welcome name='Sara' />
-        <Welcome name='Mwema' />
-        <Welcome name='Dero' />
+          <Welcome name='Sara' />
+          <Welcome name='Mwema' />
+          <Welcome name='Dero' />
         </div>
 
         <div>
-          <Comment 
-          date={comment.date}
-          text={comment.text}
-          author={comment.author}
-           />
+          <Comment
+            date={comment.date}
+            text={comment.text}
+            author={comment.author}
+          />
         </div>
-        
-        
       </body>
     </div>
   );
